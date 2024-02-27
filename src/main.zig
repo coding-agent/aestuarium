a32881c7e1bb35ea5c687bd291c2f7e866e11b3e
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-02-27 20:13:07 +0000

    wip: fixing black screen

--- src/main.zig
+++ src/main.zig
@@ -194,13 +194,6 @@ fn runMainLoop(alloc: Allocator) !u8 {
     var pixel_list = std.ArrayList(c.GLfloat).init(alloc);
     defer pixel_list.deinit();
 
-    var iterator = image.iterator();
-    while (iterator.next()) |pixel| {
-        try pixel_list.append(pixel.r);
-        try pixel_list.append(pixel.g);
-        try pixel_list.append(pixel.b);
-    }
-
     const width: c_int = @intCast(image.width);
     const height: c_int = @intCast(image.height);
     var texture: []const u8 = image.rawBytes();
@@ -239,8 +232,7 @@ fn render(texture: ?*[]const u8, width: c_int, height: c_int) void {
         \\out vec3 ourColor;
         \\out vec2 TexCoord;
         \\
-        \\void main()
-        \\{
+        \\void main() {
         \\    gl_Position = vec4(aPos, 1.0);
         \\    ourColor = aColor;
         \\    TexCoord = aTexCoord;
@@ -296,7 +288,7 @@ fn render(texture: ?*[]const u8, width: c_int, height: c_int) void {
     c.glLinkProgram(shaderProgram);
 
     const vertices = &[_]f32{
-        // positions      // colors        // texture coords
+        // positions    // colors       // texture coords
         0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
         0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
         -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom left
@@ -311,30 +303,30 @@ fn render(texture: ?*[]const u8, width: c_int, height: c_int) void {
     var VBO: c_uint = undefined;
     var EBO: c_uint = undefined;
 
-    defer c.glDeleteBuffers(1, &VAO);
-    defer c.glDeleteBuffers(1, &VBO);
-    defer c.glDeleteBuffers(1, &EBO);
-
     c.glGenVertexArrays(1, &VAO);
     c.glGenBuffers(1, &VBO);
     c.glGenBuffers(1, &EBO);
 
-    c.glBindVertexArray(VAO);
+    defer c.glDeleteVertexArrays(1, &VAO);
+    defer c.glDeleteBuffers(1, &VBO);
+    defer c.glDeleteBuffers(1, &EBO);
 
+    c.glBindVertexArray(VAO);
     c.glBindBuffer(c.GL_ARRAY_BUFFER, VBO);
-    c.glBufferData(c.GL_ARRAY_BUFFER, vertices.len * 32, vertices, c.GL_STATIC_DRAW);
+    c.glBufferData(c.GL_ARRAY_BUFFER, vertices.len * 4, vertices, c.GL_STATIC_DRAW);
 
     c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, EBO);
-    c.glBufferData(c.GL_ELEMENT_ARRAY_BUFFER, indices.len * 32, indices, c.GL_STATIC_DRAW);
+    c.glBufferData(c.GL_ELEMENT_ARRAY_BUFFER, indices.len * 4, indices, c.GL_STATIC_DRAW);
 
+    const stride = 8 * @sizeOf(f32);
     // position attribute
-    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 8 * 32, &0);
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, stride, @ptrFromInt(0));
     c.glEnableVertexAttribArray(0);
     // color attribute
-    c.glVertexAttribPointer(1, 3, c.GL_FLOAT, c.GL_FALSE, 8 * 32, &(3 * 32));
+    c.glVertexAttribPointer(1, 3, c.GL_FLOAT, c.GL_TRUE, stride, @ptrFromInt(3 * @sizeOf(f32)));
     c.glEnableVertexAttribArray(1);
     // texture coord attribute
-    c.glVertexAttribPointer(2, 2, c.GL_FLOAT, c.GL_FALSE, 8 * 32, &(6 * 32));
+    c.glVertexAttribPointer(2, 2, c.GL_FLOAT, c.GL_FALSE, stride, @ptrFromInt(6 * @sizeOf(f32)));
     c.glEnableVertexAttribArray(2);
 
     c.glUseProgram(shaderProgram);
@@ -342,7 +334,6 @@ fn render(texture: ?*[]const u8, width: c_int, height: c_int) void {
 
     var texture_id: c.GLuint = undefined;
     c.glGenTextures(1, &texture_id);
-    c.glActiveTexture(c.GL_TEXTURE0); // activate the texture unit first before binding texture
     c.glBindTexture(c.GL_TEXTURE_2D, texture_id);
 
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_WRAP_S, c.GL_CLAMP_TO_EDGE);
@@ -350,12 +341,11 @@ fn render(texture: ?*[]const u8, width: c_int, height: c_int) void {
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR_MIPMAP_LINEAR);
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
 
-    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_RGB8, width, height, 0, c.GL_RGB8, c.GL_UNSIGNED_BYTE, @ptrCast(texture));
+    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_RGB8UI, width, height, 0, c.GL_RGB, c.GL_UNSIGNED_BYTE, @ptrCast(texture));
     c.glGenerateMipmap(c.GL_TEXTURE_2D);
 
-    c.glClearColor(0.1, 0.1, 0.1, 1.0);
+    c.glClearColor(0.5, 0.5, 0.5, 1.0);
     c.glClear(c.GL_COLOR_BUFFER_BIT);
 
-    c.glDrawElements(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_BYTE, @ptrCast(&0));
-    c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+    c.glDrawElements(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_BYTE, null);
 }
